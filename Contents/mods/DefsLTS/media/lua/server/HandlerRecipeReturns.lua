ecc2d8618c9b5c37d42528b5335984c3a5ce9959
eurqa <eurqa.lord@hotmail.com>
2024-04-06 15:16:53 +0200

    Creating meat bits is now dependent on calories of the meat used

--- Contents/mods/DefsLTS/media/lua/server/HandlerRecipeReturns.lua
+++ Contents/mods/DefsLTS/media/lua/server/HandlerRecipeReturns.lua
@@ -22,3 +22,17 @@ function giveMaintenanceTools(items, result, player)
   player:getInventory():AddItem("Base.Wrench");
 end
 
+function GiveMeatBits (items,result,player)
+    local meat = nil;
+    for i=0,items:size() - 1 do
+        if instanceof(items:get(i), "Food") then
+            meat = items:get(i);
+            break;
+        end
+    end 
+    print("calories:"..meat:getCalories());
+    local meatBits = math.floor(meat:getCalories()/27)
+    for i=1,meatBits - 1 do
+        player:getInventory():AddItem("DLTS.LTSBitsMeat");
+    end
+end
